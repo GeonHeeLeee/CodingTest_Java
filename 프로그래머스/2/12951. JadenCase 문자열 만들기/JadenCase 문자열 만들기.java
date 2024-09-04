951bd23fdaebf475c5463210a1d804b8ec0a35fd
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-09-04 16:37:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 12.99 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] splitString = s.split(" ", -1);
+
+        for(String subString : splitString) {
+            if(subString.isEmpty()) {
+                answer.append(" ");
+                continue;
+            }
+            String firstString = subString.substring(0,1);
+            if(!firstString.matches("[0-9]")) {
+                firstString = firstString.toUpperCase();
+            }
+            if(subString.length() == 1) {
+                answer.append(firstString + " ");
+            } else {
+                String afterString = subString.substring(1).toLowerCase();
+                answer.append(firstString + afterString + " ");  
+            }
+        }
+
+        
+        return answer.toString().substring(0, answer.length() - 1);
+    }
+}
