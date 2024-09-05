42bd74d9c0e8d6ce663eee7d6bc3751fd824992e
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-09-05 13:47:19 +0900

    [level 2] Title: 올바른 괄호, Time: 22.72 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        char[] input = s.toCharArray();
+        Stack<Character> stack = new Stack();
+        stack.push(input[0]);
+        for(int i = 1; i < input.length; i ++) {
+            if(!stack.isEmpty() && stack.peek() == '(' && input[i] == ')') {
+                stack.pop();
+            } else {
+                stack.push(input[i]);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
