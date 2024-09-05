1bae6bdf7a075d649238a5c11ce57709f553a53a
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-09-05 13:44:43 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        int fn2 = 0;
+        int fn1 = 1;
+        int fn = fn1 + fn2;
+
+        for(int i = 1; i <= n; i ++) {
+            fn = (fn1 + fn2) % 1234567;
+            fn2 = fn1;
+            fn1 = fn;
+        }
+        
+        return fn;
+    }
+}
