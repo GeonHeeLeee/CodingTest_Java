2a779ff0f34f5e5644ce4461c75662d71ab33043
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-09-06 09:53:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.20 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int count = 0;
+        int[] answer = new int[]{0,0};
+        List<String> spokenWord = new ArrayList();
+        spokenWord.add(words[0]);
+        int personNum = 0;
+        char beforeLetter = words[0].charAt(words[0].length() - 1);
+        
+        for(int i = 1; i < words.length; i ++) {
+            personNum = (i % n) + 1;
+            char firstLetter = words[i].charAt(0);
+            if(beforeLetter == firstLetter && !spokenWord.contains(words[i])) {
+                beforeLetter = words[i].charAt(words[i].length() - 1);
+                spokenWord.add(words[i]);
+            } else {
+                count = i + 1;
+                break;
+            }
+        }
+        if(spokenWord.size() == words.length) {
+            return answer;
+        }
+        answer = new int[]{personNum, count%n == 0 ? count/n : count/n + 1};
+        return answer;
+    }
+}
