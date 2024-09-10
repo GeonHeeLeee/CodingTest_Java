ea0f0e602b10d2b3aa0aea8ae40bdcfe64bd4e0b
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-09-10 11:07:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 284.60 ms, Memory: 275 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        Deque<Character> deque = new ArrayDeque();
+        //순환
+        for(int i = 0; i < s.length(); i ++) {
+            for(int j = i; j < s.length() + i; j ++) {
+                deque.addLast(s.charAt(j % s.length()));
+            }
+            String stack = "";
+            for(int k = 0; k < s.length(); k ++) {
+                stack += Character.toString(deque.removeFirst());
+                if(stack.length() >= 2) {
+                    String subStr = stack.substring(stack.length()-2);
+                    if(subStr.equals("()") || subStr.equals("{}") || subStr.equals("[]")) {
+                        stack = stack.substring(0, stack.length() - 2);
+                    }
+                }
+            }
+            
+            if(stack.length() == 0) {
+                answer ++;
+            }
+            deque.clear();
+        }
+        return answer;
+    }
+}
