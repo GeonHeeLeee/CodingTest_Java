a7d4a96c5e8a0e4b206ba64824de8b44c41d201d
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-09-13 11:28:53 +0900

    [level 2] Title: 타겟 넘버, Time: 254.34 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,15 +1,24 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers, int target) {
-        return backTrack(numbers, target, 0, 0);
-    }
-    public int backTrack(int[] numbers, int target, int index, int sum) {
-        if(index == numbers.length) {
-            return sum == target ? 1 : 0;
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList();
+        queue.add(new int[]{0,0});
+        
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int sum = current[0];
+            int index = current[1];
+            
+            if(index == numbers.length) {
+                if(sum == target) {
+                    answer ++;
+                }
+            } else {
+                queue.add(new int[]{sum + numbers[index], index + 1});
+                queue.add(new int[]{sum - numbers[index], index + 1});
+            }
         }
-        
-        int add = backTrack(numbers, target, index + 1, sum + numbers[index]);
-        int subtract = backTrack(numbers, target, index + 1, sum - numbers[index]);
-        
-        return add + subtract;
+        return answer;
     }
 }
