52a80c334b887b7b919e1105336bbb6802b1c32c
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-09-19 13:12:51 +0900

    [level 2] Title: 모음 사전, Time: 3.08 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,16 +1,29 @@
 class Solution {
+    static int answer = 0;
+    static String alpha = "AEIOU";
+    static int cnt = 0;
+    static String[] dict = alpha.split("");
     public int solution(String word) {
-        char[] vowels = {'A', 'E', 'I', 'O','U'};
-        int[] weights = {781,156,31,6,1};
-        int answer = 0;
-        for(int i = 0; i < word.length(); i ++) {
-            char letter = word.charAt(i);
-            for(int k = 0; k < vowels.length; k ++) {
-                if(letter == vowels[k]) {
-                    answer += (k * weights[i] + 1);
-                }
+        StringBuilder sb = new StringBuilder();
+        dfs(word, 0, sb);
+        return answer;
+    }
+    public void dfs(String target, int count, StringBuilder sb) {
+        if(sb.toString().equals(target)) {
+            answer = cnt;
+            return;
+        }
+        if(count == 5) {
+            return;
+        }
+        for(int i = 0; i < dict.length; i ++) {
+            sb.append(dict[i]);
+            cnt ++;
+            dfs(target, count + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+            if(answer > 0) {
+                return;
             }
         }
-        return answer;
     }
 }
