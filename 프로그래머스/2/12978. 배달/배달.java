71054243b1c7745c6e9a3e408aa486702053afdd
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-10-15 14:22:07 +0900

    [level 2] Title: 배달, Time: 8.67 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,43 +1,44 @@
 import java.util.*;
 class Solution {
-    public int solution(int N, int[][] road, int K) {
+    public int solution(int N, int[][] r, int K) {
         int answer = 0;
+        Map<Integer, List<int[]>> graph = new HashMap();
+        for(int[] road : r) {
+            int node = road[0];
+            int dest = road[1];
+            int weight = road[2];
+            graph.computeIfAbsent(road[0], key -> new ArrayList()).add(new int[]{road[1], road[2]});
+            graph.computeIfAbsent(road[1], key -> new ArrayList()).add(new int[]{road[0], road[2]});
+        }
+        
         int[] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[1] = 0;
-
-        Map<Integer, List<int[]>> graph = new HashMap();
-        for(int i = 0; i < road.length; i ++) {
-            graph.computeIfAbsent(road[i][0], key -> new ArrayList()).add(new int[]{road[i][1], road[i][2]});
-            graph.computeIfAbsent(road[i][1], key -> new ArrayList()).add(new int[]{road[i][0], road[i][2]});
-        }
-        
         PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        queue.add(new int[]{1, 0});
+        queue.add(new int[]{1,0});
+        
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
-            int node = current[0];
-            int distance = current[1];
+            int curNode = current[0];
+            int curWeight = current[1];
             
-            if(distance > dist[node]) {
+            if(curWeight > dist[curNode]) {
                 continue;
             }
             
-            for(int[] dest : graph.get(node)) {
-                int destNode = dest[0];
-                int weight = dest[1];
+            for(int[] next : graph.get(curNode)) {
+                int nextNode = next[0];
+                int nextWeight = next[1];
                 
-                if(dist[node] + weight < dist[destNode]) {
-                    dist[destNode] = dist[node] + weight;
-                    queue.add(new int[]{destNode, dist[destNode]});
+                if(dist[curNode] + nextWeight < dist[nextNode]) {
+                    dist[nextNode] = dist[curNode] + nextWeight;
+                    queue.add(new int[]{nextNode, dist[nextNode]});
                 }
             }
-            
         }
-        for(int i = 1; i < dist.length; i ++) {
-            if(dist[i] <= K) {
+        for(int distance : dist) {
+            if(distance <= K)
                 answer ++;
-            }
         }
         return answer;
     }
