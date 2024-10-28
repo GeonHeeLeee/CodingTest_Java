6eb020a62d215f3dc55bc4ffcb8b14cec2d54063
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-10-28 15:32:16 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 1004.28 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/README.md
+++ 프로그래머스/2/62048. 멀쩡한 사각형/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀쩡한 사각형 - 62048 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/62048#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/62048) 
 
 ### 성능 요약
 
-메모리: 92.8 MB, 시간: 940.74 ms
+메모리: 79 MB, 시간: 1004.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 28일 15:27:56
+2024년 10월 28일 15:32:11
 
 ### 문제 설명
 
--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,7 +1,8 @@
+import java.math.BigInteger;
 class Solution {
     public long solution(int w, int h) {
         long answer = 0;
-        int gcd = gcd(Math.max(w, h), Math.min(w, h));
+        long gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).longValue();
         long similarityH = (long) h / gcd;
         long similarityW = (long) w / gcd;
         
@@ -12,15 +13,7 @@ class Solution {
         }
         return (long) h * w - (gcd * answer);
     }
-    public int gcd(int a, int b) {
-        while(b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-    
+
     public double function(int x, long w, long h) {
         if(x == 0) {
             return 0;
