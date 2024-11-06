57bac9ec90e659ff1f4a37563a4fa934158b46c4
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-11-06 12:40:05 +0900

    [level 3] Title: 단어 변환, Time: 1.05 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,24 +1,39 @@
+import java.util.*;
+class Word {
+    String word;
+    int count;
+    
+    public Word(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+}
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = dfs(target, words, begin, 0, new boolean[words.length]);
-        return answer == Integer.MAX_VALUE ? 0 : answer;
-    }
-    
-    public int dfs(String target, String[] words, String current, int count, boolean[] visited) {
-        if(current.equals(target)) {
-            return count;
-        }
-        int minValue = Integer.MAX_VALUE;
-        for(int i = 0; i < words.length; i ++) {
-            if(!visited[i] && isSimilar(words[i], current)) {
-                visited[i] = true;
-                int result = dfs(target, words, words[i], count + 1, visited);
-                minValue = Math.min(minValue, result);
-                visited[i] = false;
+        int answer = 0;
+        Queue<Word> queue = new LinkedList();
+        boolean[] visited = new boolean[words.length];
+        int minCount = Integer.MAX_VALUE;
+        
+        queue.add(new Word(begin, 0));
+        
+        while(!queue.isEmpty()) {
+            Word node = queue.poll();
+            String word = node.word;
+            int count = node.count;
+            if(word.equals(target)) {
+                minCount = Math.min(minCount, count);
+            }
+            for(int i = 0; i < words.length; i ++) {
+                if(isSimilar(words[i], word) && !visited[i]) {
+                    queue.add(new Word(words[i], count + 1));
+                    visited[i] = true;
+                }
             }
         }
-        return minValue;
+        return minCount == Integer.MAX_VALUE ? 0 : minCount;
     }
+    
     public boolean isSimilar(String s1, String s2) {
         char[] char1 = s1.toCharArray();
         char[] char2 = s2.toCharArray();
