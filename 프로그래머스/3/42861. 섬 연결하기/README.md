de2c8be00fcca673bd463fad1e24fa5dfecf031e
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2024-12-17 09:26:47 +0900

    [level 3] Title: 섬 연결하기, Time: 0.90 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 2.14 ms
+메모리: 86 MB, 시간: 0.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 16일 11:06:29
+2024년 12월 17일 09:26:44
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,33 +2,40 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        Map<Integer, List<int[]>> graph = new HashMap();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        boolean[] visited = new boolean[n];
-        for(int[] cost : costs) {
-            int current = cost[0];
-            int next = cost[1];
-            int weight = cost[2];
-            
-            graph.computeIfAbsent(current, key -> new ArrayList<>()).add(new int[]{next, weight});
-            graph.computeIfAbsent(next, key -> new ArrayList<>()).add(new int[]{current, weight});
+        //가중치 기준 오름차순 정렬
+        Arrays.sort(costs, (a,b) -> a[2]-b[2]);
+        
+        int[] parent = new int[n];
+        for(int idx = 0; idx < n; idx ++) {
+            parent[idx] = idx;    
         }
         
-        pq.addAll(graph.get(0));
-        visited[0] = true;
-        
-        while(!pq.isEmpty()) {
-            int[] minNode = pq.poll();
-            int next = minNode[0];
-            int weight = minNode[1];
+        for(int[] cost : costs) {
+            int start = cost[0];
+            int end = cost[1];
+            int weight = cost[2];
             
-            if(!visited[next]) {
+            if(find(parent, start) != find(parent, end)) {
+                union(parent, start, end);
                 answer += weight;
-                visited[next] = true;
-                pq.addAll(graph.get(next));
             }
         }
         
+        
         return answer;
+    }
+    
+    public int find(int[] parent, int node) {
+        if(parent[node] != node) {
+            parent[node] = find(parent, parent[node]);
+        }
+        return parent[node];
+    }
+    
+    public void union(int[] parent, int node1, int node2) {
+        int root1 = find(parent, node1);
+        int root2 = find(parent, node2);
+        
+        parent[root2] = root1;
     }
 }
