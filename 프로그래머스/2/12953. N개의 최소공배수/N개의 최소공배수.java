c314747fd625ff1dcc42ab3e84b499e0baba7bdb
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-01-05 00:09:11 +0900

    [level 2] Title: N개의 최소공배수, Time: 95.85 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -2,7 +2,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] arr) {
         int answer = 0;
-        Arrays.sort(arr);
         int min = arr[0];
         for(int i = 1; i < arr.length; i ++) {
             for(int k = min; k >= 1; k --) {
