f8398392a39fac7b451b6ed7ef8d0303f7a5243e
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-02-07 00:19:10 +0900

    [level 3] Title: 섬 연결하기, Time: 4.94 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,12 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        //가중치 기준 오름차순 정렬
-        Arrays.sort(costs, (a,b) -> a[2]-b[2]);
-        
+        // 크루스칼 알고리즘 -> 최소 신장 트리 찾기 위함
+        //간선의 가중치가 작은 것 순으로 정렬
+        Arrays.sort(costs, (a,b) -> (a[2] - b[2]));
         int[] parent = new int[n];
         for(int idx = 0; idx < n; idx ++) {
-            parent[idx] = idx;    
+            parent[idx] = idx; // 모든 노드를 독립적인 것으로 보고 자기 자신의 부모는 자기 자신으로 설정
         }
         
         for(int[] cost : costs) {
@@ -15,23 +15,26 @@ class Solution {
             int end = cost[1];
             int weight = cost[2];
             
+            //만약 시작과 끝의 부모가 같지 않으면
             if(find(parent, start) != find(parent, end)) {
-                union(parent, start, end);
-                answer += weight;
+                union(parent, start, end); //병합
+                answer += weight; //가중치 추가
             }
+            
         }
-        
-        
         return answer;
     }
     
+    //부모를 찾는 메서드
     public int find(int[] parent, int node) {
         if(parent[node] != node) {
             parent[node] = find(parent, parent[node]);
         }
+        //그니까 병합할때 자기 자신을 부모로 가지는 노드를 설정하고, 이에 병합하는 방식
         return parent[node];
     }
     
+    //병합하는 메서드
     public void union(int[] parent, int node1, int node2) {
         int root1 = find(parent, node1);
         int root2 = find(parent, node2);
