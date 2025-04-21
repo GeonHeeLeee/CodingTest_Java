f5d6458c9aae9adbf84f4a786a67bd6d9155b376
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-04-21 20:43:47 +0900

    [level 3] Title: 섬 연결하기, Time: 0.99 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,43 +1,62 @@
 import java.util.*;
+
+class Edge {
+    int node1;
+    int node2;
+    int cost;
+    
+    public Edge(int node1, int node2, int cost) {
+        this.node1 = node1;
+        this.node2 = node2;
+        this.cost = cost;
+    }
+}
 class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        // 크루스칼 알고리즘 -> 최소 신장 트리 찾기 위함
-        //간선의 가중치가 작은 것 순으로 정렬
-        Arrays.sort(costs, (a,b) -> (a[2] - b[2]));
-        int[] parent = new int[n];
-        for(int idx = 0; idx < n; idx ++) {
-            parent[idx] = idx; // 모든 노드를 독립적인 것으로 보고 자기 자신의 부모는 자기 자신으로 설정
+        List<Edge> edgeList = new ArrayList<>();
+        int[] parent = new int[n+1];
+        
+        for(int i = 1; i <= n; i ++) {
+            parent[i] = i;
         }
         
         for(int[] cost : costs) {
-            int start = cost[0];
-            int end = cost[1];
-            int weight = cost[2];
+            int node1 = cost[0];
+            int node2 = cost[1];
+            int edgeCost = cost[2];
             
-            //만약 시작과 끝의 부모가 같지 않으면
-            if(find(parent, start) != find(parent, end)) {
-                union(parent, start, end); //병합
-                answer += weight; //가중치 추가
-            }
-            
+            Edge edge = new Edge(node1, node2, edgeCost);
+            edgeList.add(edge);
         }
+        
+        Collections.sort(edgeList, (a,b) -> Integer.compare(a.cost, b.cost));
+        
+        for(Edge edge : edgeList) {
+            int node1 = edge.node1;
+            int node2 = edge.node2;
+            int cost = edge.cost;
+            
+            if(find(node1, parent) != find(node2, parent)) {
+                union(node1, node2, parent);
+                answer += cost;
+            }
+        }
+        
         return answer;
     }
     
-    //부모를 찾는 메서드
-    public int find(int[] parent, int node) {
+    public int find(int node, int[] parent) {
         if(parent[node] != node) {
-            parent[node] = find(parent, parent[node]);
+            parent[node] = find(parent[node], parent);
         }
-        //그니까 병합할때 자기 자신을 부모로 가지는 노드를 설정하고, 이에 병합하는 방식
+        
         return parent[node];
     }
     
-    //병합하는 메서드
-    public void union(int[] parent, int node1, int node2) {
-        int root1 = find(parent, node1);
-        int root2 = find(parent, node2);
+    public void union(int node1, int node2, int[] parent) {
+        int root1 = find(node1, parent);
+        int root2 = find(node2, parent);
         
         parent[root2] = root1;
     }
