da1b42b7e8e8806d53e5544722565d187d07b50b
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-04-21 18:16:45 +0900

    [level 2] Title: 구명보트, Time: 10.83 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 65.4 MB, 시간: 11.26 ms
+메모리: 55.5 MB, 시간: 10.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 05일 13:14:03
+2025년 04월 21일 18:16:42
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,16 +1,18 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int lightest = 0;
-        int heaviest = people.length - 1;
-        while(lightest <= heaviest) {
-            if(people[lightest] + people[heaviest] <= limit) {
-                lightest ++;
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right) {
+            if(people[left] + people[right] <= limit) {
+                left ++;
             }
-            heaviest --; //가장 몸무게 적은 사람이랑 해도 못타면 어차피 못타는거
-            answer++; //뚱뚱한 사람 보내기
+            right --;
+            answer ++;
         }
         return answer;
     }
