cd58bd37f5f52a80a53d6e450849fdc48b5432f9
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-04-22 23:21:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 104.51 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,27 +1,65 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minpq = new PriorityQueue<>();
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>((a,b) -> Integer.compare(b, a));
+        Map<Integer, Integer> countMap = new HashMap();
         
         for(String operation : operations) {
-            String command = operation.split(" ")[0];
-            int number = Integer.parseInt(operation.split(" ")[1]);
+            String[] input = operation.split(" ");
+            String command = input[0];
+            int number = Integer.parseInt(input[1]);
             
-            if(command.equals("I")) {
-                maxpq.offer(number);
-                minpq.offer(number);
-            } else if(!maxpq.isEmpty() && operation.equals("D 1")) {
-                minpq.remove(maxpq.poll());
-            } else if(!minpq.isEmpty() && operation.equals("D -1")) {
-                maxpq.remove(minpq.poll());
+            switch(command) {
+                case "I" :
+                    minPQ.offer(number);
+                    maxPQ.offer(number);
+                    countMap.put(number, countMap.getOrDefault(number, 0) + 1);
+                    break;
+                case "D" :
+                    if(countMap.isEmpty()) {
+                        continue;
+                    }
+                    
+                    PriorityQueue<Integer> pq = number == 1 ? maxPQ : minPQ;
+                    
+                    while(!pq.isEmpty()) {
+                        int removed = pq.poll();
+                        if(countMap.containsKey(removed)) {
+                            int count = countMap.get(removed);
+                            if(count == 1) {
+                                countMap.remove(removed);
+                            } else {
+                                countMap.put(removed, count - 1);
+                            }
+                            break;
+                        }
+                    }
             }
         }
-        if(minpq.isEmpty() && maxpq.isEmpty()) {
-            return new int[]{0,0};
-        } else {
-            return new int[]{maxpq.poll(), minpq.poll()};
+        
+        if(countMap.isEmpty()) {
+            return new int[]{0, 0};
+        }
+        
+        int max = 0;
+        int min = 0;
+        while(!minPQ.isEmpty()) {
+            int current = minPQ.poll();
+            if(countMap.containsKey(current)) {
+                min = current;
+                break;
+            }
+        }
+               
+        while(!maxPQ.isEmpty()) {
+            int current = maxPQ.poll();
+            if(countMap.containsKey(current)) {
+                max = current;
+                break;
+            }
         }
 
+        return new int[]{max, min};
     }
 }
