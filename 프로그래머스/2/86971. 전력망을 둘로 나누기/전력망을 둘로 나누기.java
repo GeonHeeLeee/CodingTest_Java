34f2ebe9866182700e42059c6bd460efe217d841
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-04-22 18:45:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.82 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,35 +1,57 @@
 import java.util.*;
+
+class Node {
+    int value;
+    List<Node> connected = new ArrayList<>();
+    
+    public Node(int value) {
+        this.value = value;
+    }
+}
 class Solution {
+    int answer = Integer.MAX_VALUE;
+    Map<Integer, Node> nodeMap = new HashMap();
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        for(int i = 0; i < wires.length; i ++) {
+        for(int i = 1; i <= n; i ++) {
+            Node node = new Node(i);
+            nodeMap.put(i, node);
+        }
+        
+        for(int[] wire : wires) {
+            Node node1 = nodeMap.get(wire[0]);
+            Node node2 = nodeMap.get(wire[1]);
+            
+            node1.connected.add(node2);
+            node2.connected.add(node1);
+        }
+        
+        for(int[] wire : wires) {
+            int start = wire[0];
+            int dest = wire[1];
+            Queue<Integer> queue = new LinkedList<>();
             boolean[] visited = new boolean[n + 1];
-            Queue<Integer> queue = new LinkedList();
-            queue.add(wires[i][0]);
-            visited[wires[i][0]] = true;
-            int count = 1;
+            queue.offer(1);
+            visited[1] = true;
+            
             while(!queue.isEmpty()) {
-                int curNode = queue.poll();
-                for(int j = 0; j < wires.length; j ++) {
-                    if(i == j) {
-                        continue;
-                    }
-                    
-                    int start = wires[j][0];
-                    int end = wires[j][1];
-                    
-                    if(start == curNode && !visited[end]) {
-                        queue.add(end);
-                        visited[end] = true;
-                        count ++;
-                    } else if(end == curNode && !visited[start]) {
-                        queue.add(start);
-                        visited[start] = true;
-                        count ++;
+                int current = queue.poll();
+                for(Node next : nodeMap.get(current).connected) {
+                    if(!visited[next.value]) {
+                        if(!(current == start && next.value == dest) && !(current == dest && next.value == start)) {
+                            queue.offer(next.value);
+                            visited[next.value] = true;
+                        }
                     }
                 }
             }
-            answer = Math.min(answer, Math.abs(count - (n - count)));
+            int count = 0;
+            for(int i = 1; i <= n; i ++) {
+                if(visited[i]) {
+                    count ++;
+                }
+            }
+            int remain = n - count;
+            answer = Math.min(answer, Math.abs(remain - count));
         }
         return answer;
     }
