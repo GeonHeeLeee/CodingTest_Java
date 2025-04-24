a66c7a12907a3815953166e04acf8b614c1172e1
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-04-25 00:27:35 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/README.md
+++ 프로그래머스/2/12936. 줄 서는 방법/README.md
@@ -1,10 +1,10 @@
 # [level 2] 줄 서는 방법 - 12936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936) 
 
 ### 성능 요약
 
-메모리: 51.9 MB, 시간: 0.06 ms
+메모리: 85.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 17:49:52
+2025년 04월 25일 00:27:33
 
 ### 문제 설명
 
--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,23 +1,20 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
-        List<Integer> people = new ArrayList();
+        List<Integer> numList = new ArrayList<>();
         long fac = 1;
         for(int i = 1; i <= n; i ++) {
+            numList.add(i);
             fac *= i;
-            people.add(i);
         }
         k --;
         for(int i = 0; i < n; i ++) {
             fac = fac / (n - i);
-            int index = (int) (k / fac);
-            answer[i] = people.remove(index);
-            k = k % fac;
+            answer[i] = numList.remove((int) (k / fac));
+            k %= fac;
         }
-
         return answer;
     }
 }
-    
-
