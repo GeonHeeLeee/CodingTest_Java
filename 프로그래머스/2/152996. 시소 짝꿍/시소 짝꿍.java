5a968d19dd7016566dcc1ce725f7c691516b51c5
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-04-24 22:32:39 +0900

    [level 2] Title: 시소 짝꿍, Time: 39.28 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -2,23 +2,35 @@ import java.util.*;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-        Map<Integer, Integer> weightMap = new HashMap();
+        TreeMap<Integer, Integer> weightMap = new TreeMap<>();
+        
         for(int weight : weights) {
             weightMap.put(weight, weightMap.getOrDefault(weight, 0) + 1);
         }
         
-        for(int weight : weightMap.keySet()) {
-            int count = weightMap.get(weight);
+        for(int key : weightMap.keySet()) {
+            int count = weightMap.get(key);
             if(count > 1) {
-                answer += (long) (count) * (count -1) / 2;
+                answer += (long) count * (count - 1) / 2;
             }
-            
-            int[] ratios = new int[]{2,3,4};
-            for(int ratio : ratios) {
-                int targetWeight = weight * (ratio) / (ratio - 1);
+        }
+        
+        int[][] ratios = new int[][]{{1,2}, {2,3}, {3,4}};
+        for(int weight : weightMap.keySet()) {
+            for(int[] ratio : ratios) {
+                int x = ratio[0];
+                int y = ratio[1];
                 
-                if(targetWeight != weight && weightMap.containsKey(targetWeight) && targetWeight * (ratio - 1) == weight * ratio) {
-                    answer += (long) count * weightMap.get(targetWeight);
+                if((weight * x) % y != 0) {
+                    continue;
+                }
+                
+                int target = (weight * x) / y;
+                if(target == weight) {
+                    continue;
+                }
+                if(weightMap.containsKey(target)) {
+                    answer += (long) weightMap.get(weight) * weightMap.get(target);
                 }
             }
         }
