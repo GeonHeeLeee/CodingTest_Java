3aa1d3332e60beadc538588845358cb58e91152e
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-04-28 21:04:23 +0900

    [Gold V] Title: 암호 만들기, Time: 120 ms, Memory: 16812 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -4,7 +4,7 @@ import java.util.*;
 class Main {
     static int l, c;
     static char[] letters;
-    static List<String> answer = new ArrayList<>();
+    static Set<String> answers = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,41 +14,45 @@ class Main {
         c = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-
         letters = new char[c];
-
         for (int i = 0; i < c; i++) {
-            letters[i] = st.nextToken().charAt(0);
-
+            char letter = st.nextToken().charAt(0);
+            letters[i] = letter;
         }
-
         Arrays.sort(letters);
-        dfs(0, 0, 0, new StringBuilder());
-        for (String s : answer) {
+        getCombinations(0, 0, 0, new StringBuilder());
+
+        List<String> answerList = new ArrayList<>(answers);
+        Collections.sort(answerList);
+        for (String s : answerList) {
             System.out.println(s);
         }
     }
 
-    public static void dfs(int index, int vowels, int consonants, StringBuilder sb) {
+    public static void getCombinations(int idx, int vCount, int cCount, StringBuilder sb) {
         if (sb.length() == l) {
-            if (vowels >= 1 && consonants >= 2) {
-                answer.add(sb.toString());
+            if (cCount >= 1 && vCount >= 2) {
+                answers.add(sb.toString());
             }
+        }
+
+        if (idx >= c) {
             return;
         }
 
-        for (int i = index; i < c; i++) {
-            sb.append(letters[i]);
-            if (isVowel(letters[i])) {
-                dfs(i + 1, vowels + 1, consonants, sb);
-            } else {
-                dfs(i + 1, vowels, consonants + 1, sb);
-            }
-            sb.deleteCharAt(sb.length() - 1);
+        char curLetter = letters[idx];
+        sb.append(curLetter);
+
+        if (isConsonant(curLetter)) {
+            getCombinations(idx + 1, vCount, cCount + 1, sb);
+        } else {
+            getCombinations(idx + 1, vCount + 1, cCount, sb);
         }
+        sb.deleteCharAt(sb.length() - 1);
+        getCombinations(idx + 1, vCount, cCount, sb);
     }
 
-    public static boolean isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    public static boolean isConsonant(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
