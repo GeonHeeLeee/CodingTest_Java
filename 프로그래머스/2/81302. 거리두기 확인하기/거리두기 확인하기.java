fdd922478905861c2b840954ec7422d1be362ff2
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-06-01 01:21:09 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.08 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -13,6 +13,7 @@ class Solution {
                         flag = true;
                         break;
                     }
+                    
                 }
                 if(flag) {
                     break;
