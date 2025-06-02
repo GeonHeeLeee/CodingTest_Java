3c2d36fc16d25f72ed16f782cb4ffe0c4d118a41
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2025-06-02 16:50:42 +0900

    [level 2] Title: 피로도, Time: 4.46 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,20 +1,20 @@
 class Solution {
-    int answer = Integer.MIN_VALUE;
+    int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        exploreDungeons(k, dungeons, new boolean[dungeons.length], 0);
+        explore(k, dungeons, new boolean[dungeons.length], 0);
         return answer;
     }
     
-    public void exploreDungeons(int curK, int[][] dungeons, boolean[] visited, int count) {
+    public void explore(int k,int[][] dungeons, boolean[] visited, int count) {
         answer = Math.max(answer, count);
         
         for(int i = 0; i < dungeons.length; i ++) {
             int required = dungeons[i][0];
             int used = dungeons[i][1];
             
-            if(!visited[i] && required <= curK) {
+            if(!visited[i] && required <= k) {
                 visited[i] = true;
-                exploreDungeons(curK - used, dungeons, visited, count + 1);
+                explore(k - used, dungeons, visited, count + 1);
                 visited[i] = false;
             }
         }
