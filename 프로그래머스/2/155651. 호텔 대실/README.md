ba092e9ea4d32aa98ec779ebd912342b86191390
GeonHee-Lee <108582570+GeonHeeLeee@users.noreply.github.com>
2026-02-19 11:12:38 +0900

    [level 2] Title: 호텔 대실, Time: 14.22 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 113 MB, 시간: 154.42 ms
+메모리: 93.7 MB, 시간: 14.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 10일 13:14:20
+2026년 02월 19일 11:12:35
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,28 +1,39 @@
 import java.util.*;
-import java.time.*;
 class Solution {
-    public int parseToMinutes(String time) {
-        int hour = Integer.parseInt(time.split(":")[0]);
-        int minutes = Integer.parseInt(time.split(":")[1]);
-        
-        return hour * 60 + minutes;
-    }
     public int solution(String[][] book_time) {
-        PriorityQueue<Integer> roomQueue = new PriorityQueue();
-        Arrays.sort(book_time, Comparator.comparing(
-            interval -> LocalTime.parse(interval[0])
-        ));
-        
+        int answer = 1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        PriorityQueue<Integer> lastest = new PriorityQueue<>();
         for(String[] time : book_time) {
-            int startTime = parseToMinutes(time[0]);
-            int endTime = parseToMinutes(time[1]) + 10;
+            String start = time[0];
+            String end = time[1];
             
-            if(!roomQueue.isEmpty() && roomQueue.peek() <= startTime) {
-                roomQueue.poll();
+            int startHour = Integer.parseInt(start.split(":")[0]) * 60;
+            int startMin = Integer.parseInt(start.split(":")[1]);
+            
+            int endHour = Integer.parseInt(end.split(":")[0]) * 60;
+            int endMin = Integer.parseInt(end.split(":")[1]);
+            
+            pq.offer(new int[]{startHour + startMin, endHour + endMin + 10});
+        }
+
+        lastest.offer(pq.poll()[1]);
+        while(!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int curStart = current[0];
+            int curEnd = current[1];
+            
+            if(!lastest.isEmpty()) {
+                if(lastest.peek() > curStart) {
+                    lastest.offer(curEnd);
+                    answer ++;
+                } else {
+                    lastest.poll();
+                    lastest.offer(curEnd);
+                }
             }
             
-            roomQueue.offer(endTime);
         }
-        return roomQueue.size();
+        return answer;
     }
 }
